16a9bfd057de257e35bdaf0b11cb99c9f90cf4e0
unknown <gustavopchriste@gmail.com>
2024-05-25 16:31:37 -0300

    projeto

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //if (html.classList.contains('light')) {
+  //html.classList.remove('light')
+  //} else {
+  //html.classList.add('light')
+  //}
+
+  if (html.classList.contains("light")) {
+  } else {
+  }
+
+  // pegar a tag img e substituir a imagem
+  // se tiver light mode, adicionar a imagem light
+  // se tiver sem LM manter a imagem normal
+}
